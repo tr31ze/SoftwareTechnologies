d0a7c39c504ee812b36013e3c8502051c9f66136
tr31ze <bobobankov@gmail.com>
2016-09-01 16:51:23 +0300

    Leave Comment component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ const App = require('react-redux-oop').App;
 const Container = require('./app/AppContainer');
 const Reducers = require('./app/AppReducers');
 
-const HomeController = require('./scripts/controllers/HomeController');
-const PostController = require('./scripts/controllers/PostController');
-const UserController = require('./scripts/controllers/UserController');
+const AppController = require('./app/AppController');
 const AuthorizationService = require('./scripts/libs/AuthorizationService');
 const Requester = require('./scripts/libs/Requester');
 
@@ -52,7 +50,7 @@ class Application extends App {
         this.baseUrl = "https://baas.kinvey.com";
         this.appKey = "kid_SJu1XCSK";
         this.appSecret = "d1fe226ed4664a33b93d726845fe9c79";
-        this._guestCredentials = "77944a02-f127-44a1-b10b-e34c12bd90c0.jh1THGUz2N4EIs3zq1Kken6fWqY/8YPm04g36B3I/fY=";
+        this._guestCredentials = "d0f1995a-bc7c-400c-a1ee-c97823ea6166.3bRdYQ8Wtk3WNNS5wV0uSubp+1yu/b/dIpLGY4PV1eM=";
 
         this._AuthService = new AuthorizationService(this.baseUrl, this.appKey, this.appSecret, this._guestCredentials);
         this._requester = new Requester(this._AuthService);
@@ -62,19 +60,15 @@ class Application extends App {
         this.selector = ".wrapper";
         this.mainContentSelector = ".main-content";
 
-        this.homeController = new HomeController(this._requester, this.baseUrl, this.appKey);
-
-        this.postController = new PostController(this._requester, this.baseUrl, this.appKey);
-
-        this.userController = new UserController(this._requester, this.baseUrl, this.appKey);
+        this.appController = new AppController(this._requester, this.baseUrl, this.appKey);
 
         initEventServices();
 
-        this._homeRouteHandler = () => this.homeController.showPage(this._AuthService.isLoggedIn(), showPopup, this._store);
+        this._homeRouteHandler = () => this.appController.showPage(this._AuthService.isLoggedIn(), showPopup, this._store);
         onRoute("#/", this._homeRouteHandler);
 
         this._singlePostHandler = (postId) => {
-            this.homeController.showSinglePost(postId, this._store);
+            this.appController.showSinglePost(postId, this._store);
         };
 
         onRoute("#/post-:id", () => {
@@ -83,29 +77,32 @@ class Application extends App {
             this._singlePostHandler(id)
         });
 
-        this._showLoginHandler = () => this.userController.showLoginPage(this._AuthService.isLoggedIn(), triggerEvent, this._store);
+        this._showLoginHandler = () => this.appController.showLoginPage(this._AuthService.isLoggedIn(), triggerEvent, this._store);
         onRoute("#/login", this._showLoginHandler);
 
-        this._showRegisterHandler = () => this.userController.showRegisterPage(this._AuthService.isLoggedIn(), triggerEvent, this._store);
+        this._showRegisterHandler = () => this.appController.showRegisterPage(this._AuthService.isLoggedIn(), triggerEvent, this._store);
         onRoute("#/register", this._showRegisterHandler);
 
-        this._logoutHandler = () => this.userController.logout(redirectUrl, this._store);
+        this._logoutHandler = () => this.appController.logout(redirectUrl, this._store);
         onRoute("#/logout", this._logoutHandler);
 
         this._showCreatePostsHandler = () => {
             let fullname = sessionStorage.getItem('fullName');
-            this.postController.showCreatePostsPage(fullname, this._AuthService.isLoggedIn(), triggerEvent, this._store);
+            this.appController.showCreatePostsPage(fullname, this._AuthService.isLoggedIn(), triggerEvent, this._store);
         };
         onRoute('#/posts/create', this._showCreatePostsHandler);
 
-        this._loginHandler = (ev, data) => this.userController.login(data, showPopup, redirectUrl, this._store);
+        this._loginHandler = (ev, data) => this.appController.login(data, showPopup, redirectUrl, this._store);
         bindEventHandler('login', this._loginHandler);
 
-        this._registerHandler = (ev, data) => this.userController.register(data, showPopup, redirectUrl, this._store);
+        this._registerHandler = (ev, data) => this.appController.register(data, showPopup, redirectUrl, this._store);
         bindEventHandler('register', this._registerHandler);
 
-        this._createPost = (ev, data) => this.postController.createPost(data, showPopup, redirectUrl, this._store);
+        this._createPost = (ev, data) => this.appController.createPost(data, showPopup, redirectUrl, this._store);
         bindEventHandler('createPost', this._createPost);
+
+        this._commentHandler = (ev, data) => this.appController.leaveComment(data, showPopup, redirectUrl, this._store);
+        bindEventHandler('leaveComment', this._commentHandler);
 
         run('#/');
 
